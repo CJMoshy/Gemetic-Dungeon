71366669274198f029c7774cd33e6c1fbb25c2ed
= <cjmoshy12@gmail.com>
2024-06-02 12:44:44 -0700

    fixed inventory, set up player spawning

---
+++ src/scenes/Intermission.ts
@@ -0,0 +1,18 @@
+import Phaser from "phaser";
+
+export default class IntermissionScene extends Phaser.Scene{
+
+    constructor(){
+        super('IntermissionScene')
+    }
+
+
+
+    init(): void{}
+    preload(): void{}
+    create(): void{}
+
+    update(time: number, delta: number): void {
+        
+    }
+}
